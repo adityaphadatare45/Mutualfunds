be6542dd6ce0c19cde15c5fce9a8b6de74578b50
unknown <adityaphadatare4545@gmail.com>
2025-01-06 17:47:37 +0530

    Few changes

--- lib/identity/identity.dart
+++ lib/identity/identity.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart'; // packages for handling the auth and database operations 
 import 'package:firebase_auth/firebase_auth.dart';   // 
 import 'package:flutter/material.dart';
-// import 'package:portfolio/screens/auth/signup.dart';
 import 'package:portfolio/screens/home.dart';
 import 'package:shared_preferences/shared_preferences.dart'; // provides persistent storage for simple data
  
