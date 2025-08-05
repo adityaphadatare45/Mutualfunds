683ed0ea7fe9101a70d594a539892acf9a8dc51d
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-05 16:30:55 +0530

    Starting personal details page

---
+++ lib/screens/identity/personaldetails.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Personaldetails extends StatefulWidget{
+  const Personaldetails({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+  
+}
