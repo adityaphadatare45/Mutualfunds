8f12b863cb7d56573cdcbbf59e53faff3c5533ad
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-05 17:19:30 +0530

    Added appbar and text

--- lib/screens/identity/personaldetails.dart
+++ lib/screens/identity/personaldetails.dart
@@ -4,10 +4,17 @@ class Personaldetails extends StatefulWidget{
   const Personaldetails({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  State<StatefulWidget> createState()=> _PersonaldetailsPage();
+}
 
-  
+class _PersonaldetailsPage extends State<Personaldetails>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+     appBar: AppBar(
+       backgroundColor: const Color.fromARGB(255, 48, 128, 194),
+       title: const Text('Personal Details'),
+     ),
+    );
+  }
 }
