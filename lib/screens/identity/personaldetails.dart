0646268cd5f164a55e7628458d72674a07b84421
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-06 19:11:05 +0530

    Did some changes in personal details page

--- lib/screens/identity/personaldetails.dart
+++ lib/screens/identity/personaldetails.dart
@@ -9,6 +9,12 @@ class Personaldetails extends StatefulWidget{
 
 class _PersonaldetailsPage extends State<Personaldetails>{
    final _formKey = GlobalKey<FormState>();
+
+
+   final _nameController = TextEditingController();
+   String? value; // Variable to hold the selected dropdown value
+   final List<String> items = ['Single', 'Joint', 'Either or Survivor']; // Dropdown items
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -28,7 +34,43 @@ class _PersonaldetailsPage extends State<Personaldetails>{
         key:_formKey,
         child: ListView(
          children: [
+
+          // Name field of user
+          TextFormField(
+           controller: _nameController,
+           decoration: InputDecoration(
+            label: Text('Full Name of Invester as per PAN *',
+            style: TextStyle(
+               fontSize: 24,
+               color: Colors.white,
+            ),
+            ),
+           ),
+          ),
+
+          const SizedBox(height: 10),
           
+          // Mode of holding drop down
+          DropdownButtonFormField(
+            value: value,
+            decoration: InputDecoration(
+              labelText: 'Mode of Holding',
+              border: OutlineInputBorder(),
+            ),
+            items: items.map((item) => DropdownMenuItem(
+              value: item,
+              child: Text(item),
+            )).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                value = newValue;
+              });
+            },
+          ),
+
+          const SizedBox(height: 10),
+
+          // Occupation drop dowm.
          ], 
         )
       ), 
