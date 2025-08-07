f466bc3f936fedbb7c6f85bd025df9100ebbfb4e
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-07 19:36:57 +0530

    Started invest details page

--- lib/screens/info/investmentdetails.dart
+++ lib/screens/info/investmentdetails.dart
@@ -9,6 +9,8 @@ class Investmentdetails extends StatefulWidget{
 }
 
  class _InvestmentPage extends State<Investmentdetails>{
+
+  String? _plantype;
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -26,10 +28,39 @@ class Investmentdetails extends StatefulWidget{
       child: Form(
         key: _formKey,
         child: ListView(
-           
+           children: [
+            _buildDropdown(
+              label: 'The Plan',
+              value: _plantype, 
+              items: [], 
+              onChanged: onChanged
+            )
+           ],
         )
         ),
       )
+    );
+  }
+  Widget _buildDropdown({
+    required String label,
+    required String? value,
+    required List<String> items,
+    required ValueChanged<String?> onChanged,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 14),
+      child: DropdownButtonFormField<String>(
+        value: value,
+        decoration: InputDecoration(
+          labelText: label,
+          border: const OutlineInputBorder(),
+        ),
+        items: items
+            .map((val) => DropdownMenuItem(value: val, child: Text(val)))
+            .toList(),
+        onChanged: onChanged,
+        validator: (val) => val == null ? 'Required' : null,
+      ),
     );
   }
 }
