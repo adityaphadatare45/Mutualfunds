4f470dc00df9a9f9b55518af903d92a7b48a51e5
adityaphadatare45 <adityaphadatare4545@gmail.com>
2025-08-22 10:23:13 +0530

    Starting today's session

--- lib/screens/info/New_sip.dart
+++ lib/screens/info/New_sip.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class NewSip extends StatefulWidget{
+  const NewSip({super.key});
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<StatefulWidget> createState() => _SipPage();
+}
+
+class _SipPage extends State<NewSip>{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    appBar: AppBar(
+      
+    ),
+   );
   }
-  
 }
